fe5935818114c8f6b66e4a04bab6b435bd2b10c3
GAURAVTARALKAR23 <taralkargaurav23@gmail.com>
2026-09-03 23:09:52 +0530

    Add Spring Boot main application class

---
+++ backend/src/main/java/com/templeapp/backend/SaptashrungiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.templeapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaptashrungiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaptashrungiBackendApplication.class, args);
+    }
+
+}
